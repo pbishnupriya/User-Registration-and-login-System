504b9a87197a8e8738614196d5b42b181a2e8bda
Bishnupriya Patra <pbishnupriya23@gmail.com>
2022-07-07 18:20:48 +0530

    Initial commit

---
+++ loginsystem/includes/footer.php
@@ -0,0 +1,13 @@
+   <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Bishnupriya Patra  </div>
+                            <div>
+                                <a href="mailto:priya.hit.84@gmail.com">Feedback</a>
+    
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
